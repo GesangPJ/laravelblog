1ff7632ef3796ba9e66cb40f45f9fcddb0a9edb4
Gesang Paudra J <g.paudra@gmail.com>
2024-06-02 15:45:25 +0700

    Update blog list UI

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,22 +6,26 @@ Halaman untuk menampilkan daftar postingan
     <h2>Blog Test</h2>
 <br>
 @foreach ($posts as $post )
+<div class="max-w-2xl p-6 overflow-hidden rounded-lg shadow dark:bg-gray-50 dark:text-gray-800">
 
-    <article class="py-8 max-w-screen-md border-b border-gray-300">
+    <article>
         <a href="/blog/{{$post['slug']}}" class="hover:underline">
-        <h3 class="mb-1 text-3xl font-bold text-grey-800">
-            {{$post['title']}}
-        </h3></a>
-        <div class="mb-1 text-base text-gray-500">
-            <a href="#">{{$post['name']}} </a>
-        </div>
-        <p class="font-light my-4">
-            {{  Str::limit($post['body'],150)}}
-        </p>
+            <h2 class="text-xl font-bold">{{$post['title']}}</h2>
+        </a>
+        <p class="mt-4 dark:text-gray-600">{{  Str::limit($post['body'],150)}}</p>
+        <br>
         <a href="/blog/{{$post['slug']}}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
-
+        <div class="flex items-center mt-8 space-x-4">
+            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="w-10 h-10 rounded-full dark:bg-gray-500">
+            <div>
+                <h3 class="text-sm font-medium">{{$post['name']}}</h3>
+                <time datetime="2021-02-18" class="text-sm dark:text-gray-600">Feb 18th 2021</time>
+            </div>
+        </div>
     </article>
-    @endforeach
+
+</div><br>
+@endforeach
 
 
 </x-layout>
